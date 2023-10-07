3ae68e05896965c0742eef67180c92a37bdb1259
YourOrdinaryCat <YourOrdinaryCat@DESKTOP-S3NJ8AN>
2023-10-07 18:06:43 -0500

    Add project files.

---
+++ src/HotCorner.Uwp/Views/MainPage.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "Views/MainPage.g.cpp"
+
+namespace wux = winrt::Windows::UI::Xaml;
+
+namespace winrt::HotCorner::Uwp::Views::implementation {
+	void MainPage::ClickHandler(const IInspectable&, const wux::RoutedEventArgs&) {
+		MyButton().Content(winrt::box_value(L"Clicked"));
+	}
+}
---
+++ src/HotCorner.Uwp/Views/MainPage.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "Views/MainPage.g.h"
+
+namespace winrt::HotCorner::Uwp::Views::implementation {
+	/**
+	 * @brief Shown to the user when the app is launched normally
+	 *        or through the tray icon.
+	*/
+	struct MainPage : MainPageT<MainPage> {
+		MainPage() {
+			// Xaml objects should not call InitializeComponent during construction.
+			// See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+		}
+
+		void ClickHandler(const Windows::Foundation::IInspectable&, const Windows::UI::Xaml::RoutedEventArgs&);
+	};
+}
+
+namespace winrt::HotCorner::Uwp::Views::factory_implementation {
+	struct MainPage : MainPageT<MainPage, implementation::MainPage> {
+	};
+}
