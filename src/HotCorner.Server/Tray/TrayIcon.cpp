1bad41d265c9f5e960d1f813543187e3e3bdbf73
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2023-10-14 16:21:05 -0500

    Added tray icon wrapper

---
+++ src/HotCorner.Server/Tray/TrayIcon.cpp
@@ -0,0 +1,59 @@
+#include "pch.h"
+#include "TrayIcon.h"
+
+#pragma comment(lib, "Shell32.lib")
+
+namespace winrt::HotCorner::Server {
+	TrayIcon::TrayIcon(const guid id) noexcept :
+		WindowBase(winrt::to_hstring(id), L"NotifyIconHost"),
+		m_id(id),
+		m_data{
+			.cbSize = sizeof(m_data),
+			.hWnd = m_window,
+			.uFlags = NIF_MESSAGE | NIF_GUID,
+			.uCallbackMessage = TrayIconCallback,
+			.uVersion = NOTIFYICON_VERSION_4,
+			.guidItem = id
+		}
+	{
+		Shell_NotifyIcon(NIM_DELETE, &m_data);
+	}
+
+	TrayIcon::~TrayIcon() noexcept {
+		Hide();
+	}
+
+	void TrayIcon::UpdateToolTip(std::wstring_view text) noexcept {
+		wcsncpy_s(m_data.szTip, text.data(), 128);
+		m_data.uFlags |= NIF_SHOWTIP | NIF_TIP;
+	}
+
+	void TrayIcon::UpdateIcon(HICON, HICON lightIcon) noexcept {
+		//TODO: Handle shell light/dark mode, and high contrast
+		m_data.hIcon = lightIcon;
+		m_data.uFlags |= NIF_ICON;
+	}
+
+	void TrayIcon::Show() noexcept {
+		m_canAdd = true;
+		if (!m_visible && Shell_NotifyIcon(NIM_ADD, &m_data)) {
+			m_visible = true;
+			Shell_NotifyIcon(NIM_SETVERSION, &m_data);
+		}
+	}
+
+	void TrayIcon::Hide() noexcept {
+		m_canAdd = false;
+		if (m_visible && Shell_NotifyIcon(NIM_DELETE, &m_data)) {
+			m_visible = false;
+		}
+	}
+
+	LRESULT TrayIcon::HandleMessage(
+		UINT message,
+		WPARAM wParam,
+		LPARAM lParam) noexcept
+	{
+		return WindowBase::HandleMessage(message, wParam, lParam);
+	}
+}
---
+++ src/HotCorner.Server/Tray/TrayIcon.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "WindowBase.h"
+#include <Unknwn.h>
+#include <shellapi.h>
+#include <winrt/base.h>
+
+namespace winrt::HotCorner::Server {
+	/**
+	 * @brief A light wrapper for a Win32 notification icon.
+	*/
+	class TrayIcon final : public WindowBase<TrayIcon> {
+		friend class WindowBase<TrayIcon>;
+
+		static constexpr uint32_t TrayIconCallback = 0xCA7;
+
+		const guid m_id;
+		NOTIFYICONDATAW m_data;
+
+		bool m_canAdd = false;
+		bool m_visible = false;
+
+	protected:
+		LRESULT HandleMessage(
+			UINT message,
+			WPARAM wParam,
+			LPARAM lParam) noexcept override final;
+
+	public:
+		TrayIcon(const guid id) noexcept;
+		~TrayIcon() noexcept;
+
+		/**
+		 * @brief Updates the icon's tooltip.
+		 *
+		 * @param text The new tooltip. Keep in mind that it will be trimmed
+		 *             to a maximum of 128 characters.
+		*/
+		void UpdateToolTip(std::wstring_view text) noexcept;
+
+		/**
+		 * @brief Updates the icon shown to the user.
+		 *
+		 * @param darkIcon A dark version of the icon, for use in light mode.
+		 * @param lightIcon A light version of the icon, for use in dark mode.
+		*/
+		void UpdateIcon(HICON darkIcon, HICON lightIcon) noexcept;
+
+		/**
+		 * @brief Whether the icon is being shown in the notification area.
+		*/
+		inline bool Visible() const noexcept {
+			return m_visible;
+		}
+
+		/**
+		 * @brief Shows the icon in the notification area. Nothing is done
+		 *        if the icon is already visible.
+		*/
+		void Show() noexcept;
+
+		/**
+		 * @brief Hides the icon from the notification area. Nothing is done
+		 *        if the icon is already hidden.
+		 *
+		 * @remarks This does not end the underlying window's message loop.
+		*/
+		void Hide() noexcept;
+	};
+}
