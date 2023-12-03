f6cca01b9bba1c5289c8a1330bd0226106434619
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2023-12-02 22:25:46 -0500

    Avoid unnecessary anonymous namespace

--- src/HotCorner.Server/Tracking/CornerTracker.cpp
+++ src/HotCorner.Server/Tracking/CornerTracker.cpp
@@ -5,190 +5,188 @@
 #include "../main.h"
 
 namespace winrt::HotCorner::Server::CornerTracker {
-	namespace {
-		std::thread m_actionThread{};
-		HANDLE m_cornerEvent = INVALID_HANDLE_VALUE;
+	std::thread m_actionThread{};
+	HANDLE m_cornerEvent = INVALID_HANDLE_VALUE;
 
-		bool m_running = false;
-		bool m_shouldRefresh = true;
+	bool m_running = false;
+	bool m_shouldRefresh = true;
 
-		std::unordered_map<std::wstring, std::array<RECT, 4>> m_displayCorners{};
-		constexpr LONG m_offset = 10;
+	std::unordered_map<std::wstring, std::array<RECT, 4>> m_displayCorners{};
+	constexpr LONG m_offset = 10;
 
-		/**
-		 * @brief Attempts to pin down the hot corner the provided point belongs to.
-		 *
-		 * @returns If the point is not within a hot corner, returns nullopt. Otherwise,
-		 *          a pair that contains the monitor ID and corner the mouse is in.
-		*/
-		std::optional<std::pair<std::wstring, ActiveCorner>> GetActiveHotCorner(const POINT& pt) {
-			for (const auto& kvp : m_displayCorners) {
-				for (uint32_t i = 0; i < kvp.second.size(); ++i) {
-					if (PtInRect(&kvp.second[i], pt)) {
-						return { { kvp.first, static_cast<ActiveCorner>(i) } };
-					}
+	/**
+	 * @brief Attempts to pin down the hot corner the provided point belongs to.
+	 *
+	 * @returns If the point is not within a hot corner, returns nullopt. Otherwise,
+	 *          a pair that contains the monitor ID and corner the mouse is in.
+	*/
+	static std::optional<std::pair<std::wstring, ActiveCorner>> GetActiveHotCorner(const POINT& pt) {
+		for (const auto& kvp : m_displayCorners) {
+			for (uint32_t i = 0; i < kvp.second.size(); ++i) {
+				if (PtInRect(&kvp.second[i], pt)) {
+					return { { kvp.first, static_cast<ActiveCorner>(i) } };
 				}
 			}
-			return std::nullopt;
 		}
+		return std::nullopt;
+	}
 
-		/**
-		 * @brief Based on the provided monitor RECT, adds monitor corner locations to
-		 *        the hot corner map.
-		*/
-		void AddCornerOffsets(std::wstring_view id, const RECT& rect) {
-			const RECT tlo{
-				.left = rect.left - m_offset,
-				.top = rect.top - m_offset,
-				.right = rect.left + m_offset,
-				.bottom = rect.top + m_offset,
-			};
+	/**
+	 * @brief Based on the provided monitor RECT, adds monitor corner locations to
+	 *        the hot corner map.
+	*/
+	static void AddCornerOffsets(std::wstring_view id, const RECT& rect) {
+		const RECT tlo{
+			.left = rect.left - m_offset,
+			.top = rect.top - m_offset,
+			.right = rect.left + m_offset,
+			.bottom = rect.top + m_offset,
+		};
 
-			const RECT tro{
-				.left = rect.right - m_offset,
-				.top = rect.top - m_offset,
-				.right = rect.right + m_offset,
-				.bottom = rect.top + m_offset,
-			};
+		const RECT tro{
+			.left = rect.right - m_offset,
+			.top = rect.top - m_offset,
+			.right = rect.right + m_offset,
+			.bottom = rect.top + m_offset,
+		};
 
-			const RECT blo{
-				.left = rect.left - m_offset,
-				.top = rect.bottom - m_offset,
-				.right = rect.left + m_offset,
-				.bottom = rect.bottom + m_offset,
-			};
+		const RECT blo{
+			.left = rect.left - m_offset,
+			.top = rect.bottom - m_offset,
+			.right = rect.left + m_offset,
+			.bottom = rect.bottom + m_offset,
+		};
 
-			const RECT bro{
-				.left = rect.right - m_offset,
-				.top = rect.bottom - m_offset,
-				.right = rect.right + m_offset,
-				.bottom = rect.bottom + m_offset,
-			};
+		const RECT bro{
+			.left = rect.right - m_offset,
+			.top = rect.bottom - m_offset,
+			.right = rect.right + m_offset,
+			.bottom = rect.bottom + m_offset,
+		};
 
-			const std::array<RECT, 4> offsets{ tlo, tro, blo, bro };
-			m_displayCorners.insert_or_assign(id.data(), offsets);
-		}
+		const std::array<RECT, 4> offsets{ tlo, tro, blo, bro };
+		m_displayCorners.insert_or_assign(id.data(), offsets);
+	}
 
-		/**
-		 * @brief Enumerates the currently connected displays, and replaces the current
-		 *        monitor corner locations.
-		*/
-		void RefreshDisplays() {
-			m_displayCorners.clear();
+	/**
+	 * @brief Enumerates the currently connected displays, and replaces the current
+	 *        monitor corner locations.
+	*/
+	static void RefreshDisplays() {
+		m_displayCorners.clear();
 
-			DISPLAY_DEVICE display{};
+		DISPLAY_DEVICE display{};
+		ZeroMemory(&display, sizeof(display));
+		display.cb = sizeof(display);
+
+		DEVMODE dm{};
+
+		DWORD index = 0;
+		while (EnumDisplayDevices(NULL, index, &display, 0)) {
+			// This is needed. Why? I have no idea, but it only works if I do this
+			std::array<WCHAR, 32> name{};
+			wcscpy_s(name.data(), name.size(), display.DeviceName);
+
+			// Only add displays that are part of the desktop
+			if (display.StateFlags & DISPLAY_DEVICE_ATTACHED_TO_DESKTOP) {
+				if (!EnumDisplayDevices(name.data(), 0, &display, EDD_GET_DEVICE_INTERFACE_NAME)) {
+					//TODO: Handle failure
+					OutputDebugString(L"Failed to get display ID\n");
+					goto next_display;
+				}
+
+				ZeroMemory(&dm, sizeof(DEVMODE));
+				dm.dmSize = sizeof(DEVMODE);
+
+				if (!EnumDisplaySettings(name.data(), ENUM_CURRENT_SETTINGS, &dm)) {
+					//TODO: Handle failure
+					OutputDebugString(L"Failed to get screen rect\n");
+					goto next_display;
+				}
+
+				const RECT screenRect{
+					.left = dm.dmPosition.x,
+					.top = dm.dmPosition.y,
+					.right = dm.dmPosition.x + static_cast<LONG>(dm.dmPelsWidth),
+					.bottom = dm.dmPosition.y + static_cast<LONG>(dm.dmPelsHeight)
+				};
+
+				AddCornerOffsets(display.DeviceID, screenRect);
+			}
+
+		next_display:
 			ZeroMemory(&display, sizeof(display));
 			display.cb = sizeof(display);
-
-			DEVMODE dm{};
-
-			DWORD index = 0;
-			while (EnumDisplayDevices(NULL, index, &display, 0)) {
-				// This is needed. Why? I have no idea, but it only works if I do this
-				std::array<WCHAR, 32> name{};
-				wcscpy_s(name.data(), name.size(), display.DeviceName);
-
-				// Only add displays that are part of the desktop
-				if (display.StateFlags & DISPLAY_DEVICE_ATTACHED_TO_DESKTOP) {
-					if (!EnumDisplayDevices(name.data(), 0, &display, EDD_GET_DEVICE_INTERFACE_NAME)) {
-						//TODO: Handle failure
-						OutputDebugString(L"Failed to get display ID\n");
-						goto next_display;
-					}
-
-					ZeroMemory(&dm, sizeof(DEVMODE));
-					dm.dmSize = sizeof(DEVMODE);
-
-					if (!EnumDisplaySettings(name.data(), ENUM_CURRENT_SETTINGS, &dm)) {
-						//TODO: Handle failure
-						OutputDebugString(L"Failed to get screen rect\n");
-						goto next_display;
-					}
-
-					const RECT screenRect{
-						.left = dm.dmPosition.x,
-						.top = dm.dmPosition.y,
-						.right = dm.dmPosition.x + static_cast<LONG>(dm.dmPelsWidth),
-						.bottom = dm.dmPosition.y + static_cast<LONG>(dm.dmPelsHeight)
-					};
-
-					AddCornerOffsets(display.DeviceID, screenRect);
-				}
-
-			next_display:
-				ZeroMemory(&display, sizeof(display));
-				display.cb = sizeof(display);
-				++index;
-			}
+			++index;
 		}
+	}
 
-		/**
-		 * @brief This thread runs when the cursor enters the hot corner, and waits for
-		 *        its reset event with a timeout equivalent to the desired delay. If the
-		 *        wait times out, the corner function is executed. If the object is signaled,
-		 *        the thread exits.
-		*/
-		void OnHotCornerEntry(std::pair<std::function<bool()>, uint32_t> param) noexcept {
-			const auto result = WaitForSingleObject(m_cornerEvent, param.second);
-			if (result == WAIT_TIMEOUT) {
-				if (!param.first()) {
-					//TODO: Handle failure
-					OutputDebugString(L"Failed to perform user-defined action\n");
-				}
-			}
-			else if (result == WAIT_FAILED) {
+	/**
+	 * @brief This thread runs when the cursor enters the hot corner, and waits for
+	 *        its reset event with a timeout equivalent to the desired delay. If the
+	 *        wait times out, the corner function is executed. If the object is signaled,
+	 *        the thread exits.
+	*/
+	static void OnHotCornerEntry(std::pair<std::function<bool()>, uint32_t> param) noexcept {
+		const auto result = WaitForSingleObject(m_cornerEvent, param.second);
+		if (result == WAIT_TIMEOUT) {
+			if (!param.first()) {
 				//TODO: Handle failure
-				OutputDebugString(L"Failed to wait for hot corner event\n");
+				OutputDebugString(L"Failed to perform user-defined action\n");
 			}
 		}
+		else if (result == WAIT_FAILED) {
+			//TODO: Handle failure
+			OutputDebugString(L"Failed to wait for hot corner event\n");
+		}
+	}
 
-		std::optional<Settings::MonitorSettings> GetSettingsById(std::wstring_view id) {
-			static const auto& saved = Current::Settings().Monitors;
-			const auto stored = std::find_if(saved.begin(), saved.end(), [&id](const Settings::MonitorSettings& monitor)
-				{
-					return monitor.Id == id;
-				}
-			);
-
-			if (stored != saved.end() && stored->Enabled) {
-				return *stored;
+	static std::optional<Settings::MonitorSettings> GetSettingsById(std::wstring_view id) {
+		static const auto& saved = Current::Settings().Monitors;
+		const auto stored = std::find_if(saved.begin(), saved.end(), [&id](const Settings::MonitorSettings& monitor)
+			{
+				return monitor.Id == id;
 			}
-			return std::nullopt;
+		);
+
+		if (stored != saved.end() && stored->Enabled) {
+			return *stored;
+		}
+		return std::nullopt;
+	}
+
+	static std::optional<std::function<bool()>> GetAction(const Settings::MonitorSettings& settings, ActiveCorner corner) noexcept {
+		switch (corner) {
+		case ActiveCorner::TopLeft:
+			return Tracking::GetDelegate(settings.TopLeftAction);
+		case ActiveCorner::TopRight:
+			return Tracking::GetDelegate(settings.TopRightAction);
+		case ActiveCorner::BottomLeft:
+			return Tracking::GetDelegate(settings.BottomLeftAction);
+		case ActiveCorner::BottomRight:
+			return Tracking::GetDelegate(settings.BottomRightAction);
 		}
 
-		std::optional<std::function<bool()>> GetAction(const Settings::MonitorSettings& settings, ActiveCorner corner) noexcept {
-			switch (corner) {
-			case ActiveCorner::TopLeft:
-				return Tracking::GetDelegate(settings.TopLeftAction);
-			case ActiveCorner::TopRight:
-				return Tracking::GetDelegate(settings.TopRightAction);
-			case ActiveCorner::BottomLeft:
-				return Tracking::GetDelegate(settings.BottomLeftAction);
-			case ActiveCorner::BottomRight:
-				return Tracking::GetDelegate(settings.BottomRightAction);
-			}
+		return std::nullopt;
+	}
 
-			return std::nullopt;
+	static uint32_t GetDelay(const Settings::MonitorSettings& settings, ActiveCorner corner) noexcept {
+		if (!settings.DelayEnabled) {
+			return 0;
 		}
 
-		uint32_t GetDelay(const Settings::MonitorSettings& settings, ActiveCorner corner) noexcept {
-			if (!settings.DelayEnabled) {
-				return 0;
-			}
-
-			switch (corner) {
-			case ActiveCorner::TopLeft:
-				return settings.TopLeftDelay;
-			case ActiveCorner::TopRight:
-				return settings.TopRightDelay;
-			case ActiveCorner::BottomLeft:
-				return settings.BottomLeftDelay;
-			case ActiveCorner::BottomRight:
-				return settings.BottomRightDelay;
-			}
-
-			return std::numeric_limits<uint32_t>::max();
+		switch (corner) {
+		case ActiveCorner::TopLeft:
+			return settings.TopLeftDelay;
+		case ActiveCorner::TopRight:
+			return settings.TopRightDelay;
+		case ActiveCorner::BottomLeft:
+			return settings.BottomLeftDelay;
+		case ActiveCorner::BottomRight:
+			return settings.BottomRightDelay;
 		}
+
+		return std::numeric_limits<uint32_t>::max();
 	}
 
 	StartupResult Start(HWND window) noexcept {
