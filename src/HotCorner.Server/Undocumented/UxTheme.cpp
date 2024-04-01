41a6de5236ee31f79e9d27c2b8f7c625b704b038
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2024-04-01 18:39:15 -0500

    fix: avoid ShouldSystemUseDarkMode on builds under 1903

--- src/HotCorner.Server/Undocumented/UxTheme.cpp
+++ src/HotCorner.Server/Undocumented/UxTheme.cpp
@@ -1,10 +1,16 @@
 #include "pch.h"
 #include "UxTheme.h"
 
+#include <Version.h>
 #include <wil/resource.h>
 
 namespace winrt::HotCorner::Server::Undocumented {
 	ShellTheme GetCurrentShellTheme() noexcept {
+		// Before 1903, the default shell theme was dark, no light mode
+		if (!IsAtLeast1903()) {
+			return ShellTheme::Dark;
+		}
+
 		// https://github.com/TranslucentTB/TranslucentTB/blob/0726b0afbdf83eb579537d59bb36076a9071ec06/TranslucentTB/dynamicloader.hpp#L26C4-L26C4
 		static wil::unique_hmodule m_UxTheme{};
 		m_UxTheme.reset(LoadLibraryEx(L"uxtheme.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32));
