6ad999d3cec895d03ce7a6ee73d042336bf90936
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2024-04-13 20:04:07 -0500

    feat: add initial control (API shape only, no implementation yet)

---
+++ src/HotCorner.Uwp/Controls/WrapPanel.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+#include "WrapPanel.h"
+#include "Controls/WrapPanel.g.cpp"
+
+namespace winrt::HotCorner::Uwp::Controls::implementation {
+}
---
+++ src/HotCorner.Uwp/Controls/WrapPanel.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <Utils/Property.h>
+#include <Utils/Xaml.h>
+
+#include "Controls/WrapPanel.g.h"
+
+namespace winrt::HotCorner::Uwp::Controls::implementation {
+	using namespace XamlAliases;
+
+	struct WrapPanel : WrapPanelT<WrapPanel> {
+		WrapPanel() { }
+
+		Windows::Foundation::Size MeasureOverride(Windows::Foundation::Size);
+		Windows::Foundation::Size ArrangeOverride(Windows::Foundation::Size) const;
+
+		DEPENDENCY_PROPERTY_META(
+			Orientation,
+			wuxc::Orientation,
+			box_value(wuxc::Orientation::Vertical),
+			nullptr
+		);
+
+		DEPENDENCY_PROPERTY_META(
+			HorizontalSpacing,
+			double,
+			box_value(0.0),
+			nullptr
+		);
+
+		DEPENDENCY_PROPERTY_META(
+			VerticalSpacing,
+			double,
+			box_value(0.0),
+			nullptr
+		);
+
+	private:
+		Windows::Foundation::Size m_maxSize{};
+	};
+}
+
+namespace winrt::HotCorner::Uwp::Controls::factory_implementation {
+	struct WrapPanel : WrapPanelT<WrapPanel, implementation::WrapPanel> {
+	};
+}
